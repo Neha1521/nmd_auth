9a60aa8a83e25127681c3512268b8e02d146e378
Neha1521 <curdle39@gmail.com>
2021-01-02 22:24:07 +0530

    Mail OTP during every login using random OTP

---
+++ app/src/main/java/com/example/nmd_auth/PassCheckActivity.java
@@ -0,0 +1,44 @@
+package com.example.nmd_auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PassCheckActivity extends AppCompatActivity {
+
+    private EditText otp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_pass_check);
+
+        otp = findViewById(R.id.etOtp);
+        Button next = findViewById(R.id.btnNext);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String actualOtp = getIntent().getStringExtra("OTP");
+                String userOtp = otp.getText().toString();
+                if(!userOtp.isEmpty() && userOtp.equals(actualOtp)){
+                    Toast.makeText(PassCheckActivity.this, "Successs",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(PassCheckActivity.this, "Wrong OTP",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+}
